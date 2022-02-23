05defe81034ca8cff261ff1806877cdaf1f13390
nyafnir <leonid.b4@yandex.ru>
2022-02-23 20:51:02 +0700

    fix: "vv", cpu usage not work (remove)

--- src/commands/Разработка/mira.ts
+++ src/commands/Разработка/mira.ts
@@ -2,33 +2,6 @@ import { Client, CommandInteraction, MessageEmbed } from 'discord.js';
 import { timeFomattedDHMS, roundDecimalPlaces } from '@utils';
 import { config } from '@config';
 import { ArgType } from '@services/commander';
-import { cpus } from 'os';
-
-const ncpu = cpus().length;
-let previousTime = new Date().getTime();
-let previousUsage = process.cpuUsage();
-let lastUsage = {
-    system: -1,
-    total: -1,
-    user: -1,
-};
-
-// @daniele-ricci https://stackoverflow.com/a/63331219/9063899
-setInterval(() => {
-    const currentUsage = process.cpuUsage(previousUsage);
-    previousUsage = process.cpuUsage();
-
-    const currentTime = new Date().getTime();
-    const timeDelta = (currentTime - previousTime) * ncpu;
-    const { user, system } = currentUsage;
-
-    lastUsage = {
-        system: system / timeDelta,
-        total: (system + user) / timeDelta,
-        user: user / timeDelta,
-    };
-    previousTime = currentTime;
-}, 1000);
 
 module.exports = {
     name: __filename.slice(__dirname.length + 1).split('.')[0],
@@ -76,18 +49,18 @@ module.exports = {
                                 process.memoryUsage().heapUsed / 8e6,
                                 0,
                             ) +
-                            ' / ' +
+                            '/' +
                             roundDecimalPlaces(
                                 process.memoryUsage().rss / 8e6,
                                 0,
                             ) +
                             ' МБ'
-                        }\`\nЦП: \`${lastUsage.total}\``,
+                        }\`\nЦП: \`--%\``,
                         inline: true,
                     },
                     {
                         name: 'Статистика',
-                        value: `Сервера: **${client.guilds.cache.size.toString()}** Версия: [v${
+                        value: `Сервера: **${client.guilds.cache.size.toString()}** Версия: [${
                             config.bot.version
                         }](${config.bot.homepage})`,
                         inline: false,
